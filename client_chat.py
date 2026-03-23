99240b139c7c6e98b38697d9ca6ab1727b66a3ae
assiabenkiniouar808 <benkiniouarassia@gmail.com>
2026-03-23 16:39:45 +0100

    Update client_chat.py

--- client_chat.py
+++ client_chat.py
@@ -1,33 +1,16 @@
 import socket
-import threading
 
 MAX_BYTES = 65535
 
-def recevoir(sock):
-    while True:
-        try:
-            data, address = sock.recvfrom(MAX_BYTES)
-            print("\nMessage reçu :", data.decode('utf-8'))
-        except:
-            break
-
 def client(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    thread_reception = threading.Thread(target=recevoir, args=(sock,), daemon=True)
-    thread_reception.start()
-
-    print("Tape tes messages. Tape 'quit' pour quitter.")
-
     while True:
-        text = input("> ")
+        text = input('Votre message : ')
+        data = text.encode('ascii')
+        sock.sendto(data, ('127.0.0.1', port))
+        data, address = sock.recvfrom(MAX_BYTES)
+        text = data.decode('ascii')
+        print('Message recu : {}'.format(text))
 
-        if text.lower() == "quit":
-            break
-
-        sock.sendto(text.encode('utf-8'), ('127.0.0.1', port))
-
-    sock.close()
-
-if __name__ == "__main__":
+if _name_ == '_main_':
     client(1060)
