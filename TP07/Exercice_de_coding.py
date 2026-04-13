04210aa4d3a4ce6fa21de6c395011a5294e5f9e0
assiabenkiniouar808 <benkiniouarassia@gmail.com>
2026-04-13 16:08:25 +0200

    Create Exercice_de_coding.py

---
+++ TP07/Exercice_de_coding.py
@@ -0,0 +1,19 @@
+def merge(nums1, m, nums2, n):
+    i = m - 1      # fin des éléments utiles de nums1
+    j = n - 1      # fin de nums2
+    k = m + n - 1  # fin de nums1 total
+
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+    
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
